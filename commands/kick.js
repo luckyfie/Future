b2d2afa98cf70b2904934de88997a9569625e743
luckyfie <thibe.beerens@gmail.com>
2021-09-12 14:02:53 +0200

    init
    init 2

---
+++ commands/kick.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: 'kick-user',
+    description: 'Kicks an user',
+    aliases: ['kick'],
+    permissions: ['KICK_MEMBERS'],
+    execute(message, args, cmd, client, Discord){
+        const target = message.mentions.users.first();
+
+        let memberTarget = message.guild.members.cache.get(target.id)
+        const kicker = memberTarget.kick()
+
+        if (!args[1]) {
+
+            message.channel.send("De opgegeven gebruiker is verbannen van de **Server****!**");
+
+            message.guild(memberTarget, {
+                userDeleter
+            })
+        }
+    }
+}
--- commands/message.js
+++ commands/message.js
@@ -4,7 +4,7 @@ module.exports = {
     aliases: ['probleem', 'hostingdown'],
     permissions: ['MANAGE_MESSAGES'],
     execute(message, args, cmd, client, Discord) {
-        const scripter = "Beste @here! \n\nDe server ondervindt enkele problemen met de vatse hosting. Hier is echter niet veel aan te doen. We wachten rustig af tot reactie van de Hosting. \n\nMet Vriendelijke Groeten\nNederbelgen Development"
+        const scripter = "Beste @here! \n\nDe problemen met de host zijn opgelost. U kunt terug spelen! type connect 185.227.243.25:30250 in F8! \n\nMet Vriendelijke Groeten\nNederbelgen Development"
 
         message.channel.send(scripter)
         message.delete()
