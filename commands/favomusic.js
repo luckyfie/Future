90971ced5e1fb2a575befae579214249695c6f65
luckyfie <thibe.beerens@gmail.com>
2021-09-12 16:59:09 +0200

    Create favomusic.js
    Update

---
+++ commands/favomusic.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'favomusic',
+    description: 'favomusic from the owner',
+    permissions: [],
+    execute(message, args, client) {
+        message.channel.send("***Dit is de favorite music van de owner.***\n\n**- Stairway to Heaven**\n**- Måneskin - Beggin (LYRICS)**\n**- Queen - The Show Must Go On (Official Video)**\n**- I Can't Believe**\n**- Schatje mag ik je foto?**\n**- liever te dik in de kist**\n**")
+    }
+}
