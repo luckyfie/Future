bbac077aa5857b5041a14625fbc81c47ad99eeae
luckyfie <thibe.beerens@gmail.com>
2021-12-10 11:28:48 +0100

    Update addRole.js

--- commands/addRole.js
+++ commands/addRole.js
@@ -4,21 +4,25 @@ module.exports = {
     name: 'add',
     description: 'adds a user from a channel',
     aliases: ['Y', 'N'],
-    permissions: ['MOVE_MEMBERS', 'MANAGE_CHANNELS'],
+    permissions: ['ADMINSTRATOR'],
     execute(message, args, client, Discord) {
         const target = message.mentions.users.first();
-        let memberTarget = message.guild.members.cache.get(target.id)
+        let memberTarget = message.guild.members.cache.get(target.id);
 
-        const giveRole = message(!args[2])
+        const giveRole = message(!args[2]);
 
         if (!args[2]) {
-            message.channel.send(`Gaat het om gebruiker ${memberTarget} en role ${giveRole}? (reply met ***,Y or ,N***)`)
+            message.channel.send(`Gaat het om gebruiker ${memberTarget} en role ${giveRole}? (reply met ***,Y or ,N***)`);
         }
 
         else if (cmd === 'Y') {
-            memberTarget.roles.add(giveRole)
+            memberTarget.roles.add(giveRole);
         }
 
+        else if (cmd === 'N') {
+            let messageF = `We zullen ${memberTarget} de role ${giveRole} niet geven!` 
+            message.reply()
+        }
     }
 
 }
