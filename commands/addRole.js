c4e306cf458ad1923cbff3516639053f45a203fa
luckyfie <thibe.beerens@gmail.com>
2021-12-10 11:34:00 +0100

    Update addRole.js

--- commands/addRole.js
+++ commands/addRole.js
@@ -9,7 +9,8 @@ module.exports = {
         const target = message.mentions.users.first();
         let memberTarget = message.guild.members.cache.get(target.id);
 
-        const giveRole = message(!args[2]);
+        const mRole = message(!args[2]);
+        let giveRole = message.guild.roles.cache.find(r => r.id === mRole); 
 
         if (!args[2]) {
             message.channel.send(`Gaat het om gebruiker ${memberTarget} en role ${giveRole}? (reply met ***,Y or ,N***)`);
