4c3dec87c582e78e078ed604f8e04eaaa045d324
luckyfie <thibe.beerens@gmail.com>
2022-01-03 14:18:55 +0100

    Fivem Server

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 
+
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
@@ -10,10 +11,17 @@ client.events = new Discord.Collection();
 })
 
 
+// Server Status
+const { Client } = require('discord.js');
+const { updatePlayerCount } = require("./utils/")
+global.config = require("./config.json")
 
+client.on('ready', () => {
+    console.log(`Discord Bot logged in als ${client.user.tag}`);
+    updatePlayerCount(client, 5)
+})
 
-
-
+// Einde server status
 
 client.login(process.env.token);
 
@@ -22,3 +30,5 @@ client.login(process.env.token);
 // ODYxMTc1NDAyNTcwNTc5OTY5.YOF-Fw.hlvZKY59e5Bp6OdrMb_pQrRncFY
 
 
+// invite your bot
+// https://discordapp.com/oauth2/authorize?client_id=your_client_id&scope=bot&permissions=1099511627775
