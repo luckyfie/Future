3d853ad647f2239899c9d554fb69c808ffb3836c
luckyfie <thibe.beerens@gmail.com>
2022-02-05 15:46:58 +0100

    NTokenAnymoreTosee

--- commands/fmessage.js
+++ commands/fmessage.js
@@ -1,19 +1,22 @@
 module.exports = {
-    name: 'say',
+    name: 'fmesay',
     description: 'Zegt onderstaande bericht',
     aliases: ['zeg'],
     permissions: [],
     execute(message, args, cmd, client, discord) {
-        const channel = message.guild.channels.cache.find(c => c.name === '『💼』uitslagen');
-        
+
+        let channelUitslagen = message.guild.channels.cache.find(c => c.name === '『💼』uitslagen');
+
+        let channelMention = message.guild.channels.cache.get('889518832727785522').toString();
+
         const messageFormulierenHulpdienstenEmbed = new discord.MessageEmbed()
         .setColor('00feff')
         .setAuthor('ZevenDijk RolePlay')
-        .setDescription(`Je kunt solliciteren bij een hulpdienst doormiddel van een ticket aan te maken in ${889518832727785522} veel succes!`);
+        .setDescription(`Je kunt solliciteren bij een hulpdienst doormiddel van een ticket aan te maken in  ${channelMention}, veel succes!`);
 
         const channel = message.guild.channels.cache.find(c => c.name === '『💼』uitslagen');
 
-        channel.send(messageFormulierenHulpdienstenEmbed).then((msg) => {
+        channelUitslagen.send(messageFormulierenHulpdienstenEmbed).then((msg) => {
             message.delete();
         }).catch((err) => {
             throw err;
