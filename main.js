76af53668308218c5b429b2790602580eb8772f4
luckyfie <thibe.beerens@gmail.com>
2022-08-21 13:10:24 +0200

    Update main.js

--- main.js
+++ main.js
@@ -18,36 +18,6 @@ client.events = new Discord.Collection();
 client.login(process.env.token);
 
 // process.env.token
-// dO U SSE,
+
 // MTAwOTUwMTc5MTU4ODcyODg3Mw.GWsSjo.SAI6T-OOzvwpY-qpxoWsG6w-GMuwz2rpGJG62Q
-
-////////////////////////////////////////////////////////////////////
-
-// const Discord = require('discord.js')
-
-// var prefix = '!';
-
-// const client = new Discord.Client()
-
-// // const client = new Discord.Client({
-// //     allowedMentions: {
-// //         parse: [`users`, `roles`],
-// //         repliedUser: true
-// //     },
-
-// //     intents: [
-// //         "GUILDS",
-// //         "GUILD_MESSAGES",
-// //         "GUILD_PRESENCES",
-// //         "GUILD_MEMBERS",
-// //         "GUILD_MESSAGE_REACTIONS"
-// //     ]
-
-// // });
-
-// client.on("ready", () => {
-//     console.log("Bot is online!")
-
-// });
-
 // client.login("MTAxMDE2NjY4MTM4NzEzOTEyMg.GP_FL4.uCfgrAoo52M-FICjOyAbtE53JqTmxIIYchEwqA")
